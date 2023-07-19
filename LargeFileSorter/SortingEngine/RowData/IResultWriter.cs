23a4844b44e46ad64b37a187d8f9f2f0438bc0eb
Igor Solovyev <igor.solovyev@kofax.com>
2023-07-19 21:48:52 +0200

    added comment

--- LargeFileSorter/SortingEngine/RowData/IResultWriter.cs
+++ LargeFileSorter/SortingEngine/RowData/IResultWriter.cs
@@ -3,9 +3,12 @@
 namespace SortingEngine.RowData;
 
 /// <summary>
-/// An object implementing this interface will write a piece of information to a file and close it afterward. 
+/// An object implementing this interface will write a piece of information to a file and close it afterward.
+/// This interface is used only inside the core part  
 /// </summary>
-public interface IOneTimeLinesWriter
+public interface 
+    
+    IOneTimeLinesWriter
 {
     Result WriteRecords(string filePath, Line[] lines, int linesNumber, ReadOnlyMemory<byte> source);
     Task<Result> WriteRecordsAsync(string filePath, Line[] lines, int linesNumber, ReadOnlyMemory<byte> source,
@@ -14,6 +17,7 @@ public interface IOneTimeLinesWriter
 
 /// <summary>
 /// An object implementing this interface can write several times in the same file.
+/// This is a contract between this internal sorting engine and the external application sink of the data. 
 /// </summary>
 public interface ISeveralTimesLinesWriter : IDisposable, IAsyncDisposable
 {
